6960b1b1e144b49b01d49f3eaf6fd7102381c9e9
shubham9076 <erkrshubham912068@gmail.com>
2026-06-05 15:17:38 +0530

    Add files via upload

---
+++ day_8/halfpyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+    int n,i,j;
+    printf("enter a row number ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
