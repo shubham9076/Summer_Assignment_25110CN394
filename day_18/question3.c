5213df8748ff14ce34d49f0f463aea7d677f0fe2
shubham9076 <erkrshubham912068@gmail.com>
2026-06-30 00:07:02 +0530

    Add files via upload

---
+++ day_18/question3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, low, high, mid;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
